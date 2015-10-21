fbd0993f694b7d6a81e886164b8f15bbca17d887
Chakery <chakery@sina.com>
2015-10-21 17:22:07 +0800

    Initial Commit

---
+++ AVFoundationQRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVFoundationQRCode
+//
+//  Created by qf on 15/10/21.
+//  Copyright (c) 2015年 Chakery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
